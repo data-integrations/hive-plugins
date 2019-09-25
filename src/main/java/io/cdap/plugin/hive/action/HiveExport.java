95d2b3b4f7ee1ee523acc8952a52f2cd3b601e8c
agensitskiy <agensitskiy@cybervisiontech.com>
2019-09-25 15:13:15 +0300

    CDAP-15842: Hive-plugins plugin validation.

--- src/main/java/io/cdap/plugin/hive/action/HiveExport.java
+++ src/main/java/io/cdap/plugin/hive/action/HiveExport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017 Cask Data, Inc.
+ * Copyright © 2017-2019 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -19,21 +19,13 @@ package io.cdap.plugin.hive.action;
 import io.cdap.cdap.api.annotation.Description;
 import io.cdap.cdap.api.annotation.Name;
 import io.cdap.cdap.api.annotation.Plugin;
+import io.cdap.cdap.etl.api.FailureCollector;
 import io.cdap.cdap.etl.api.PipelineConfigurer;
 import io.cdap.cdap.etl.api.action.Action;
 import io.cdap.cdap.etl.api.action.ActionContext;
 import io.cdap.plugin.hive.action.common.HiveCommandExecutor;
-import org.apache.calcite.sql.SqlNode;
-import org.apache.calcite.sql.SqlSelect;
-import org.apache.calcite.sql.parser.SqlParseException;
-import org.apache.calcite.sql.parser.SqlParser;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 
 /**
  * Hive Export runs a select query against a hive table and stores results under an hdfs directory.
@@ -51,52 +43,24 @@ public class HiveExport extends Action {
 
   @Override
   public void configurePipeline(PipelineConfigurer pipelineConfigurer) throws IllegalArgumentException {
-    validateExportConfig();
-  }
-
-  private void validateExportConfig() {
-    if (!config.containsMacro("statement")) {
-      // validate hive command. For export we only accept Select statements
-      SqlParser parser = SqlParser.create(config.statement);
-      try {
-        SqlNode sqlNode = parser.parseQuery();
-        if (!(sqlNode instanceof SqlSelect)) {
-          throw new IllegalArgumentException("Hive Export only uses Select statements. Please provide valid hive " +
-                                               "select statement.");
-        }
-      } catch (SqlParseException e) {
-        throw new IllegalArgumentException("Error while parsing select statement. Please provide a valid hive select " +
-                                             "statement.");
-      }
-    }
-
-    if (!config.containsMacro("overwrite")) {
-      // validate if the directory already exists
-      if (config.overwrite.equalsIgnoreCase("no")) {
-        Configuration configuration = new Configuration();
-        try {
-          FileSystem fs = FileSystem.get(configuration);
-          if(fs.exists(new Path(config.path))) {
-            throw new IllegalArgumentException(String.format("The path %s already exists. Please either delete that " +
-                                                               "path or provide another path.", config.path));
-          }
-        } catch (IOException e) {
-          throw new RuntimeException("Exception occurred while doing directory check", e);
-        }
-      }
-    }
+    FailureCollector failureCollector = pipelineConfigurer.getStageConfigurer().getFailureCollector();
+    config.validate(failureCollector);
   }
 
   @Override
   public void run(ActionContext context) throws Exception {
-    validateExportConfig();
+    FailureCollector failureCollector = context.getFailureCollector();
+    config.validate(failureCollector);
+    failureCollector.getOrThrowException();
+
     // Create Insert command for hive
-    String command = "INSERT OVERWRITE DIRECTORY '" + config.path +
-      "' row format delimited  FIELDS TERMINATED BY '" + config.delimiter + "' " + config.statement;
+    String command = "INSERT OVERWRITE DIRECTORY '" + config.getPath() +
+      "' row format delimited  FIELDS TERMINATED BY '" + config.getDelimiter() + "' " + config.getStatement();
 
     LOG.debug("Hive command being executed: {}", command);
 
-    HiveCommandExecutor executor = new HiveCommandExecutor(config.connectionString, config.user, config.password);
+    HiveCommandExecutor executor = new HiveCommandExecutor(config.getConnectionString(), config.getUser(),
+                                                           config.getPassword());
     executor.execute(command);
     executor.cleanup();
   }
